8a16bbf2deeb9df8c87390ea1e0d8b47f2d7fa1a
Dishitha0607 <dishi0607@gmail.com>
2026-04-07 18:29:13 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItems from "./components/TodoItems";
+import { useState } from "react";
+import WelcomeMessage from "./components/WelcomeMessage";
+
+function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+    console.log(`New item added: ${itemName} with due date ${itemDueDate}`);
+    const newTodoItems = [
+      ...todoItems,
+      { name: itemName, dueDate: itemDueDate },
+    ];
+    setTodoItems(newTodoItems);
+  };
+
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter(item => item.name !== todoItemName);
+    setTodoItems(newTodoItems)
+    console.log(`Delete item: ${todoItemName}`);
+  };
+
+  return (
+    <>
+      <center className="todo-container">
+        <AppName />
+        <AddTodo onNewItem={handleNewItem} />
+        {todoItems.length === 0 && <WelcomeMessage />}
+        <TodoItems todoItems={todoItems} onDeleteClick={handleDeleteItem} />
+      </center>
+    </>
+  );
+}
+
+export default App;
