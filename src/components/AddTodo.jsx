404afa844444ecf2aafba5d404af30d6a4ad2939
Dishitha0607 <dishi0607@gmail.com>
2026-04-07 18:30:07 +0530

    Add files via upload

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+function AddTodo({ onNewItem }) {
+  const [todoName, setTodoName] = useState("");
+  const [todoDueDate, setTodoDueDate] = useState("");
+
+  const handleNameChange = (event) => {
+    setTodoName(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setTodoDueDate(event.target.value);
+  };
+
+  const handleAddButtonClicked = () => {
+    onNewItem(todoName, todoDueDate);
+    setTodoDueDate("");
+    setTodoName("");
+  };
+
+  return (
+    <div className="container">
+      <div className="row kg-row">
+        <div className="col-6">
+          <input
+            type="text"
+            placeholder="Enter Todo Here"
+            value={todoName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div className="col-4">
+          <input type="date" value={todoDueDate} onChange={handleDateChange} />
+        </div>
+        <div className="col-2">
+          <button
+            type="button"
+            className="btn btn-success kg-button"
+            onClick={handleAddButtonClicked}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddTodo;
---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,15 @@
+import TodoItem from "./TodoItem";
+import styles from "./TodoItems.module.css";
+const TodoItems = ({ todoItems, onDeleteClick }) => {
+  return (
+    <>
+      <div className={styles["items-container"]}>
+        {todoItems.map((item) => (
+          <TodoItem todoDate={item.dueDate} todoName={item.name} onDeleteClick={onDeleteClick}></TodoItem>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default TodoItems;
